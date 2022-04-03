700545ea94cd344a522e5ffc6b50f704a170a0ae
Abhishek <abhisheklpophale123@gmail.com>
2022-04-03 17:05:48 +0530

    smart contract deployed successfully

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xb3b147ACaD0cD491F12cb4c49fB03E328296Cde3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
